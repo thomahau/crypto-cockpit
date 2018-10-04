70a18f7649ed85a2b753b0442d7b586e7a199e71
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2018-10-04 13:16:38 +0200

    header layout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import './App.css';
 
-const CustomElement = styled.div`
-  color: green;
+const AppLayout = styled.div`
+  padding: 40px;
+  display: grid;
+  grid-template-columns: 180px auto 100px 100px;
 `;
+
+const Logo = styled.div`
+  font-size: 1.5em;
+`;
+
+const ControlButton = styled.div``;
 
 class App extends Component {
   render() {
-    return <CustomElement>Hello</CustomElement>;
+    return (
+      <AppLayout>
+        <Logo>CryptoCockpit</Logo>
+        <div />
+        <ControlButton>Dashboard</ControlButton>
+        <ControlButton>Settings</ControlButton>
+      </AppLayout>
+    );
   }
 }
 
